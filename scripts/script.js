ef389c5015322be2b3b51b2148c6ed9d86178046
rococorbalan <123517270+rococorbalan@users.noreply.github.com>
2024-10-10 14:38:05 -0300

    Add input for changing work and break times.

--- scripts/script.js
+++ scripts/script.js
@@ -2,22 +2,60 @@ let countdown;
 let timeLeft;
 
 const timerDisplay = document.getElementById('timer');
-const startButton = document.getElementById('startButton');
-const pauseButton = document.getElementById('pauseButton')
+const startButton = document.getElementById('start-button');
+const pauseButton = document.getElementById('pause-button')
 const statusDisplay = document.getElementById('status');
 
-//let time = parseFloat(prompt("Minutes")) * 60;
-let time = 5;
-let breakTime = 7;
+let time = 1500;
+let breakTime = 300;
+timeFormat(time);
 
 let isBreak = false;
 let isWork = true;
+let isTimerOn = false;
 
-timeFormat(time);
+let workInput = document.getElementById('work-input'); 
+let breakInput = document.getElementById('break-input')
 
 timeLeft = time;
+
+workInput.addEventListener('input', () => {
+    if(workInput.value != ''){
+        workTime = workInput.value;
+        if (isTimerOn) {
+            timeFormat(workTime);
+            timeLeft = workTime;
+        }
+    }else{
+        workTime = 300
+        if (isTimerOn) {
+            timeFormat(workTime);
+            timeLeft = workTime;
+        }
+    }
+});
+
+breakInput.addEventListener('input', () => {
+    if(breakInput.value != ''){
+        breakTime = breakInput.value;
+        if (isTimerOn) {
+            timeFormat(breakTime);
+            timeLeft = breakTime;
+        }
+    }else{
+        breakTime = 300
+        if (isTimerOn) {
+            timeFormat(breakTime);
+            timeLeft = breakTime;
+        }
+    }
+});
+
+//let time = parseFloat(prompt("Minutes")) * 60;
+
 startButton.addEventListener('click', startTimer);
 function startTimer() {
+    isTimerOn = true;
     clearInterval(countdown);
     timeFormat(timeLeft);
 
@@ -31,6 +69,7 @@ function startTimer() {
             if(isWork){
                 isWork = false;
                 isBreak = true;
+                isTimerOn = false;
 
                 timeFormat(breakTime);
                 timeLeft = breakTime;
@@ -40,6 +79,7 @@ function startTimer() {
             }else if (isBreak){
                 isWork = true;
                 isBreak = false;
+                isTimerOn = false;
 
                 timeFormat(time);
                 timeLeft = time;
@@ -52,7 +92,7 @@ function startTimer() {
 }
 
 
-pauseButton.addEventListener('click', ()=> {clearInterval(countdown)});
+pauseButton.addEventListener('click', ()=> {clearInterval(countdown); isTimerOn = false;});
 
 
 function timeFormat(seconds){
