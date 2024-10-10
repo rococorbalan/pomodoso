4997e1f1d8cb3abe9d0143997367293994d25730
rococorbalan <123517270+rococorbalan@users.noreply.github.com>
2024-10-10 15:49:23 -0300

    Add restart and skip buttons; change start/pause
    The start/pause buttons now work as one with a
    toggle button. Center the elements on the page.

--- scripts/script.js
+++ scripts/script.js
@@ -2,15 +2,18 @@ let countdown;
 let timeLeft;
 
 const timerDisplay = document.getElementById('timer');
-const startButton = document.getElementById('start-button');
-const pauseButton = document.getElementById('pause-button')
+
+const toggleButton = document.getElementById('toggle-button');
+const restartButton = document.getElementById('restart-button');
+const skipButton = document.getElementById('skip-button');
+
 const statusDisplay = document.getElementById('status');
 
 let time = 1500;
 let breakTime = 300;
 timeFormat(time);
 
-let isBreak = false;
+
 let isWork = true;
 let isTimerOn = false;
 
@@ -21,16 +24,19 @@ timeLeft = time;
 
 workInput.addEventListener('input', () => {
     if(workInput.value != ''){
-        workTime = workInput.value;
+        time = workInput.value;
         if (isTimerOn) {
-            timeFormat(workTime);
-            timeLeft = workTime;
+            timeFormat(time);
+            timeLeft = time;
+        }else if(!isTimerOn && isWork){
+            timeFormat(time);
+            timeLeft = time;
         }
     }else{
-        workTime = 300
+        time = 1500
         if (isTimerOn) {
-            timeFormat(workTime);
-            timeLeft = workTime;
+            timeFormat(time);
+            timeLeft = time;
         }
     }
 });
@@ -51,10 +57,40 @@ breakInput.addEventListener('input', () => {
     }
 });
 
-//let time = parseFloat(prompt("Minutes")) * 60;
 
-startButton.addEventListener('click', startTimer);
+toggleButton.addEventListener('click', () => {
+    if(isTimerOn){
+        pauseTimer();
+    }else{
+        startTimer();
+    } 
+});
+
+
+restartButton.addEventListener('click', () => {
+    clearInterval(countdown);
+    if (isTimerOn){
+        toggleButton.textContent = "Start Timer"
+        isTimerOn = false;
+    }
+
+    if(isWork) {
+        timeLeft = time;
+        timeFormat(timeLeft);
+    }else{
+        timeLeft = breakTime;
+        timeFormat(timeLeft);
+    }
+})
+
+
+skipButton.addEventListener('click', () => {
+    switchWork();
+})
+
+
 function startTimer() {
+    toggleButton.textContent = "Pause Timer"
     isTimerOn = true;
     clearInterval(countdown);
     timeFormat(timeLeft);
@@ -66,47 +102,69 @@ function startTimer() {
         if(timeLeft <= 0){
             clearInterval(countdown);
             alert("Time's up!")
-            if(isWork){
-                isWork = false;
-                isBreak = true;
-                isTimerOn = false;
-
-                timeFormat(breakTime);
-                timeLeft = breakTime;
-                clearInterval(countdown);
-
-                statusDisplay.textContent = "BREAK";
-            }else if (isBreak){
-                isWork = true;
-                isBreak = false;
-                isTimerOn = false;
-
-                timeFormat(time);
-                timeLeft = time;
-                clearInterval(countdown);
-
-                statusDisplay.textContent = "WORK";
-            }
+            switchWork();
         }
     }, 1000);
 }
 
 
-pauseButton.addEventListener('click', ()=> {clearInterval(countdown); isTimerOn = false;});
+function switchWork(){
+    if(isWork){
+        isWork = false;
+        isTimerOn = false;
+        toggleButton.textContent = "Start Timer"
+
+        timeFormat(breakTime);
+        timeLeft = breakTime;
+        clearInterval(countdown);
+
+        statusDisplay.textContent = "BREAK";
+    }else if (!isWork){
+        isWork = true;
+        isTimerOn = false;
+        toggleButton.textContent = "Start Timer"
+
+        timeFormat(time);
+        timeLeft = time;
+        clearInterval(countdown);
+
+        statusDisplay.textContent = "WORK";
+    }
+}
+
+
+function pauseTimer(){
+    clearInterval(countdown); 
+    isTimerOn = false;
+    toggleButton.textContent = "Start Timer"
+}
 
 
 function timeFormat(seconds){
-    const hours = Math.floor(seconds / 3600);
-    const minutes = Math.floor((seconds % 3600) / 60);
-    const secs = Math.floor(seconds % 60);
+    if(seconds >= 3600){
+        const hours = Math.floor(seconds / 3600);
+        const minutes = Math.floor((seconds % 3600) / 60);
+        const secs = Math.floor(seconds % 60);
 
-    const formattedTime = [
-        String(hours).padStart(2, '0'),
-        String(minutes).padStart(2, '0'),
-        String(secs).padStart(2, '0')
-    ].join(':');
+        const formattedTime = [
+            String(hours).padStart(2, '0'),
+            String(minutes).padStart(2, '0'),
+            String(secs).padStart(2, '0')
+        ].join(':');
 
-    timerDisplay.textContent = formattedTime;
+        timerDisplay.textContent = formattedTime;
+    }else {
+        const minutes = Math.floor(seconds / 60);
+        const secs = Math.floor(seconds % 60);
+
+        const formattedTime = [
+            String(minutes).padStart(2, '0'),
+            String(secs).padStart(2, '0')
+        ].join(':');
+
+        timerDisplay.textContent = formattedTime;
+    }
+    
 }
 
 
